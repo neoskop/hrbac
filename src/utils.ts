d7709781f9cdf05d36ab4da72e3e4326f45e7242
Mark Wecke <wecke@neoskop.de>
2017-09-15 09:17:50 +0200

    fix(tests): fix coverage

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 export function objectEntries<T>(object: { [key: string]: T }) : [ string, T ][] {
     /* istanbul ignore else */
     if(Object.entries) {
+        /* istanbul ignore next */
         return Object.entries(object);
     }
+    /* istanbul ignore next */
     return Object.keys(object).map<[ string, T ]>(key => [ key, object[key] ]);
 }
